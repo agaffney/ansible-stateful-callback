4e322651ffe09b79207aefd4c2eeea1fadc69907
Andrew Gaffney <andrew@agaffney.org>
2018-02-15 10:42:02 -0600

    make it configurable

--- callback_plugins/stateful.py
+++ callback_plugins/stateful.py
@@ -15,7 +15,23 @@ class CallbackModule(CallbackBase):
 
 
     def v2_runner_on_ok(self, result):
-        if result._task.action == 'file':
-            if not 'file_paths' in result._host.vars:
-                result._host.vars['file_paths'] = []
-            result._host.vars['file_paths'].append(result._result['dest'])
+        task_vars = result._task.get_variable_manager().get_vars(host=result._host, task=result._task, play=result._task._parent._play)
+        task_args = result._task.args
+        stateful_config = task_vars.get('ansible_stateful_config', [])
+        if isinstance(stateful_config, dict):
+            stateful_config = [stateful_config]
+        for config in stateful_config:
+            for capture in config['capture']:
+                if result._task.action == capture['action']:
+                    field_from = capture.get('from', 'args')
+                    value = None
+                    if field_from == 'args':
+                        if capture['field'] in task_args:
+                            value = task_args[capture['field']]
+                    elif field_from == 'result':
+                        if capture['field'] in result._result:
+                            value = result._result[capture['field']]
+                    if value:
+                        if not config['var_name'] in result._host.vars:
+                            result._host.vars[config['var_name']] = []
+                        result._host.vars[config['var_name']].append(value)
